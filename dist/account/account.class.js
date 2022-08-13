295dcd42da2e079c468cb6c5b70fd7fef12b0150
VictorNery <victorcndp@gmail.com>
2022-08-12 23:47:24 -0300

    docs: update .gitignore

---
+++ dist/account/account.class.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Account = void 0;
+class Account {
+    constructor(account_number, agency) {
+        this.account_number = account_number;
+        this.agency = agency;
+        this.balance = 0;
+    }
+}
+exports.Account = Account;
